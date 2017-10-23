986d0267c66e371370f5786ffe753ee374c2268a
tintran <12520438@gm.uit.edu.vn>
2017-10-23 16:11:37 +0700

    a

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -114,11 +114,10 @@ class CustomerController extends Controller
 	        $this->data['page'] = $page->withFakes();
 	        $this->data['menu'] = MenuItem::all();
 	        $this->data['cart'] = Cart::content();
-	        $this->data['cartOld'] = Cart::content();
 	        $user = Auth::user();
          	$customer_id = $user->connected_id;
-			if($user->email == 'cau2binhdinh@gmail.com' || $user->email == 'minh.huynh@cfarm.vn'){
-				$orders = DB::select('SELECT g.`order_id` "order_id", g.`total` "total", g.`created_at` "date", s.`name` "status_name", s.`vn_name` "status_vn_name", g.`status` "status", g.`discount_amount` "discount_amount", g.`note` "note", g.`shipping_cost` "shipping_cost", g.`customer_id` "customer_id" FROM `g_orders` g, `status` s WHERE g.`status` = s.`id` ORDER BY g.`order_id` DESC');
+			if($user->email == 'minh.huynh@cfarm.vn'){
+				$orders = DB::select('SELECT g.`order_id` "order_id", g.`total` "total", g.`created_at` "date", s.`name` "status_name", s.`vn_name` "status_vn_name", g.`status` "status", g.`discount_amount` "discount_amount", g.`note` "note", g.`shipping_cost` "shipping_cost", g.`customer_id` "customer_id", g.`delivery_date` "delivery_date" FROM `g_orders` g, `status` s WHERE g.`status` = s.`id` ORDER BY g.`order_id` DESC');
 				$this->data['orders'] = $orders;
 				$this->data['orderItem'] = [];
 				foreach ($orders as $order) {
@@ -139,30 +138,36 @@ class CustomerController extends Controller
 		        // return $this->data['orderItem'];
 		        return view('pages.user2', $this->data);
 			}else{
-				$orders = DB::select('SELECT g.`order_id` "order_id", g.`total` "total", g.`created_at` "date", s.`name` "status_name", s.`vn_name` "status_vn_name", g.`status` "status", g.`discount_amount` "discount_amount", g.`note` "note", g.`shipping_cost` "shipping_cost" FROM `g_orders` g, `status` s WHERE g.`status` = s.`id` AND g.`customer_id` = ? ORDER BY g.`order_id` DESC', [$customer_id]);
+				$orders = DB::select('SELECT g.`order_id` "order_id", g.`total` "total", g.`created_at` "date", s.`name` "status_name", s.`vn_name` "status_vn_name", g.`status` "status", g.`discount_amount` "discount_amount", g.`note` "note", g.`shipping_cost` "shipping_cost", g.`delivery_date` "delivery_date" FROM `g_orders` g, `status` s WHERE g.`status` = s.`id` AND g.`customer_id` = ? ORDER BY g.`order_id` DESC', [$customer_id]);
 			}
 			
 			$this->data['orders'] = $orders;
 			$this->data['orderItem'] = [];
+	        $this->data['orderRate'] = [];
+	        $today = date("Y-m-d");
+	        $week = strtotime(date("Y-m-d", strtotime($today)) . " -1 week");
+  			$week = strftime("%Y-%m-%d", $week);
+  			// return $week;
 			foreach ($orders as $order) {
-				$item = DB::select('SELECT p.`name` "product_name", m.`quantity` "quantity", m.`unit` "unit", m.`price` "price", f.`name` "farmer_name" FROM `m_orders` m, `products` p, `farmers` f  
+				$item = DB::select('SELECT p.`id` "id", p.`name` "product_name", m.`quantity` "quantity", m.`unit` "unit", m.`price` "price", f.`name` "farmer_name" FROM `m_orders` m, `products` p, `farmers` f  
 					WHERE m.`product_id` = p.`id` AND f.`id` = m.`farmer_id` AND m.`order_id` = ?', [$order->order_id]);
 				// array_push($this->data['orderItem'], $item);
+				if(empty($this->data['orderRate'] ) && $order->delivery_date > $week){
+					$this->data['orderRate'] = $item;
+				}
 				$this->data['orderItem'][$order->order_id] = $item;
 				$this->data['orderItem'][$order->order_id]['shipping_cost'] = $order->shipping_cost;
 				$this->data['orderItem'][$order->order_id]['discount_amount'] = $order->discount_amount;
 			}
 	        // $this->data['cartOld'] = DB::table('articles')->where('id', $post_id)->first();
 	        // return $this->data['orderItem'];
-	        if($user->email == 'cau2binhdinh@gmail.com'){
-	        	return view('pages.user3', $this->data);
-	        }
+	        
 	        return view('pages.user', $this->data);
 		}
 		return redirect()->back();
 	}
 
-	public function rate(Request $request)
+	public function rating(Request $request)
 	{
 		return $data = $request->data;
 	}
