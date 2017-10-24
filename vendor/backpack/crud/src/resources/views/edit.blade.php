96113b481681c0e29351cde7e377f145ef0f316f
tintran <12520438@gm.uit.edu.vn>
2017-10-24 15:59:32 +0700

    a

--- vendor/backpack/crud/src/resources/views/edit.blade.php
+++ vendor/backpack/crud/src/resources/views/edit.blade.php
@@ -14,6 +14,7 @@
 @endsection
 
 @section('content')
+@if($crud->entity_name_plural != "orders")
 <div class="row">
 	<div class="col-md-8 col-md-offset-2">
 		<!-- Default box -->
@@ -61,4 +62,124 @@
 		  {!! Form::close() !!}
 	</div>
 </div>
+@else
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+		<!-- Default box -->
+		@if ($crud->hasAccess('list'))
+			<a href="{{ url($crud->route) }}"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
+		@endif
+
+		@include('crud::inc.grouped_errors')
+
+		  {!! Form::open(array('url' => $crud->route.'/'.$entry->getKey(), 'method' => 'put', 'files'=>$crud->hasUploadFields('update', $entry->getKey()))) !!}
+		  <div class="box">
+		    <div class="box-header with-border">
+		    	@if ($crud->model->translationEnabled())
+			    	<!-- Single button -->
+					<div class="btn-group pull-right">
+					  <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					    {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} <span class="caret"></span>
+					  </button>
+					  <ul class="dropdown-menu">
+					  	@foreach ($crud->model->getAvailableLocales() as $key => $locale)
+						  	<li><a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}?locale={{ $key }}">{{ $locale }}</a></li>
+					  	@endforeach
+					  </ul>
+					</div>
+					<h3 class="box-title" style="line-height: 30px;">{{ trans('backpack::crud.edit') }}</h3>
+				@else
+					<h3 class="box-title">{{ trans('backpack::crud.edit') }}</h3>
+				@endif
+		    </div>
+		    <div class="box-body row">
+		      <!-- load the view from the application if it exists, otherwise load the one in the package -->
+		      @if(view()->exists('vendor.backpack.crud.form_content'))
+		      	@include('vendor.backpack.crud.form_content', ['fields' => $fields, 'action' => 'edit'])
+		      @else
+		      	@include('crud::form_content', ['fields' => $fields, 'action' => 'edit'])
+		      @endif
+		    </div><!-- /.box-body -->
+
+            <div class="box-footer">
+
+                @include('crud::inc.form_save_buttons')
+
+		    </div><!-- /.box-footer-->
+		  </div><!-- /.box -->
+		  {!! Form::close() !!}
+
+		  <table id="crudTable" class="table table-bordered table-striped display">
+            <thead>
+              <tr>
+                <th>STT</th>
+                <th>Sản phẩm</th>
+                <th>Số lượng</th>
+                <th>Đơn vị</th>
+                <th>Thành tiền</th>
+                <th>Trang trại</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              @if (!$crud->ajaxTable())
+                @foreach ($entries as $k => $entry)
+                <tr data-entry-id="{{ $entry->getKey() }}">
+
+                  @if ($crud->details_row)
+                    @include('crud::columns.details_row_button')
+                  @endif
+
+                  {{-- load the view from the application if it exists, otherwise load the one in the package --}}
+                  @foreach ($crud->columns as $column)
+                    @if (!isset($column['type']))
+                      @include('crud::columns.text')
+                    @else
+                      @if(view()->exists('vendor.backpack.crud.columns.'.$column['type']))
+                        @include('vendor.backpack.crud.columns.'.$column['type'])
+                      @else
+                        @if(view()->exists('crud::columns.'.$column['type']))
+                          @include('crud::columns.'.$column['type'])
+                        @else
+                          @include('crud::columns.text')
+                        @endif
+                      @endif
+                    @endif
+
+                  @endforeach
+
+                  @if ($crud->buttons->where('stack', 'line')->count())
+                    <td>
+                      @include('crud::inc.button_stack', ['stack' => 'line'])
+                    </td>
+                  @endif
+
+                </tr>
+                @endforeach
+              @endif
+
+            </tbody>
+            <tfoot>
+              <tr>
+                @if ($crud->details_row)
+                  <th></th> <!-- expand/minimize button column -->
+                @endif
+
+                {{-- Table columns --}}
+                @foreach ($crud->columns as $column)
+                  <th>{{ $column['label'] }}</th>
+                @endforeach
+
+                @if ( $crud->buttons->where('stack', 'line')->count() )
+                  <th>{{ trans('backpack::crud.actions') }}</th>
+                @endif
+              </tr>
+            </tfoot>
+          </table>
+
+
+	</div>
+</div>
+@endif
 @endsection
